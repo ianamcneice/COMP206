a377a591a148c3e93018b0a60b3979aaa751beb3
Ian MCNEICE <imcnei1@teaching.CS.McGill.CA>
2015-03-23 09:58:43 -0400

    First commit of list.h and list.c

---
+++ list.c
@@ -0,0 +1,2 @@
+#include <std.io>
+#include <list.h>
---
+++ list.h
@@ -0,0 +1,12 @@
+#ifndef
+#define LIST_H
+
+#define BOOLEAN int
+#define TRUE 1
+#define FALSE 0
+
+void add(int);
+void prettyPrint();
+BOOLEAN delete(int);
+
+#endif
