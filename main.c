eb178c00927070eb8050f5e99c84bf4e78ac8d82
Ian MCNEICE <imcnei1@teaching.CS.McGill.CA>
2015-03-23 11:18:34 -0400

    Will take user input, but doesn't recognize correct files.

--- main.c
+++ main.c
@@ -1,1 +1,28 @@
 #include <stdio.h>
+#include "list.h"
+
+int main(void){
+	
+	//declare char array to hold user input and boolean for while loop
+	char f_name[100];
+	BOOLEAN exists = FALSE;
+	FILE *fptr;
+
+	while(!exists){
+
+		printf("Please enter a filename or path: "); 
+		fptr = fgets(f_name, 100, stdin);
+	
+		//check if file exists and open;
+		if(fptr = fopen(f_name, "r")){
+			exists = TRUE;
+			//debugging
+			printf("File exists");
+		}
+		else
+			printf("FILE OR PATH INVALID. ");
+	}
+	
+
+	fclose(fptr);
+}
