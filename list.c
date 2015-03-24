e42b3c8d7c702053eb38e8dfbdb854e4d763e5e3
Ian MCNEICE <imcnei1@teaching.CS.McGill.CA>
2015-03-23 22:25:25 -0400

    First LL implementation - compiling errors.

--- list.c
+++ list.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
 #include "list.h"
+
+//LL
+struct elem{
+	int value;
+	struct elem *next;
+};
+
+typedef struct elem NODE;
+
+NODE *head, *tail;
+
+//functions
+void add(int num){
+	tail = (NODE *)malloc(sizeof(NODE));
+	tail->value = num;
+	tail->next = NULL;
+	if(head == NULL)
+		head = tail;
+}
+
+void prettyPrint(){
+	NODE *curr;
+	curr = head;
+	while(curr !=NULL){
+		printf("%d %s", curr->value, ", ");
+		curr = curr->next;
+}
+
+//BOOLEAN delete(int){
+
+//}
+
+//struct NODE *find(int){
+
+//}
